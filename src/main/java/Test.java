977c0baf6ecc7e84029feb317092060faef079c8
git-chendon <a13223066862>
2019-10-11 21:16:19 +0800

    encoding

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Test {
-
-
-
-
-
+    public static void main(String[] args) {
+        int[] xx = new int[]{1, 2, 3};
+        List<int[]> aa = new ArrayList<int[]>();
+        aa.add(xx);
+//        for(int i=0; i<aa.size(); i++) {
+//            System.out.println("列表项为: "+i);
+//        }
+        int[] zz= aa.get(0);
+        for (int i =0 ;i<zz.length;i++)
+        System.out.println(zz[i]);
+    }
 }
